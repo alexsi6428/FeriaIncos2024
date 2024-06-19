ab3845e825b9d41798d6270983414185da91ac7b
DanielaRPsc <darosales2023@incos-santacruz.edu.bo>
2024-06-19 12:12:59 -0400

    Se agrego la Seccion_Reciclabes, una shape en drawable, string titulo y color para shape
    Signed-off-by: DanielaRPsc <darosales2023@incos-santacruz.edu.bo>

---
+++ app/src/main/java/com/example/feria/Seccion_Reciclabes_Activity.java
@@ -0,0 +1,14 @@
+package com.example.feria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Seccion_Reciclabes_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seccion_reciclabes);
+    }
+}
