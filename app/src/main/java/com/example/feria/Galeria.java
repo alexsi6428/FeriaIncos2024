37a97bc8d4b0e513ee4a94c22881e766a9c4c091
fercho <fpaniagua2023@incos-santacruz.edu.bo>
2024-06-21 14:37:35 -0400

    avance de mi

---
+++ app/src/main/java/com/example/feria/Galeria.java
@@ -0,0 +1,16 @@
+package com.example.feria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ViewFlipper;
+
+public class Galeria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galeria);
+
+    }
+}
