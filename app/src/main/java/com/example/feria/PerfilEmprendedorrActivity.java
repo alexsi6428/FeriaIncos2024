385f1fe85308ba822590223b8800d62f571c5048
DanielaRPsc <darosales2023@incos-santacruz.edu.bo>
2024-06-21 22:43:20 -0400

    Actilizado

--- app/src/main/java/com/example/feria/PerfilEmprendedorrActivity.java
+++ app/src/main/java/com/example/feria/PerfilEmprendedorrActivity.java
@@ -2,13 +2,24 @@ package com.example.feria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PerfilEmprendedorrActivity extends AppCompatActivity {
-
+    Button btnperfilemprendedor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_emprendedorr);
+        btnperfilemprendedor=findViewById(R.id.btnperfilemprendedor);
+        btnperfilemprendedor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRD=new Intent(PerfilEmprendedorrActivity.this, ProductosEmprendedorActivity.class);
+                startActivity(intentRD);
+            }
+        });
     }
 }
