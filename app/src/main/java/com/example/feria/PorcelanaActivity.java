9e85c38abb10016c96a396f4462a10d2042ef1f5
Alex <asilva2023@gmail.com>
2024-06-21 23:00:41 -0400

    terminado seccion compra no reciclar y info no reciclar y los intent

---
+++ app/src/main/java/com/example/feria/PorcelanaActivity.java
@@ -0,0 +1,14 @@
+package com.example.feria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PorcelanaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_porcelana);
+    }
+}
