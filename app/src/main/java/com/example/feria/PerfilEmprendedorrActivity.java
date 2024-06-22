9381cdb4f962b9df8ddfe3df0442c2aade3bf135
DanielaRPsc <darosales2023@incos-santacruz.edu.bo>
2024-06-21 22:27:24 -0400

    perfil emprendedor

---
+++ app/src/main/java/com/example/feria/PerfilEmprendedorrActivity.java
@@ -0,0 +1,14 @@
+package com.example.feria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PerfilEmprendedorrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_emprendedorr);
+    }
+}
